44ee7e9c31170c67417fd1fa5510bd425fa0dcbb
hyun6 <psymhs@gmail.com>
2022-11-12 23:24:13 +0900

    feat: parse meta data simplification
     - just pass file parameter
     - original read file buffer method caused by tauri file drop handler has only file path, not file object

--- packages/renderer/src/feature/utils/parseMetadata.ts
+++ packages/renderer/src/feature/utils/parseMetadata.ts
@@ -5,22 +5,7 @@ import { parseBlob } from 'music-metadata-browser';
  * 미디어 파일에서 메타 정보를 추출한다
  */
 export const parseMetadataFromFile = async (file: File): Promise<IAudioMetadata | undefined> => {
-  return new Promise((resolve, reject) => {
-    const readStart = performance.now();
-    const fileReader = new FileReader();
-    fileReader.onload = async function (ev: ProgressEvent<FileReader>) {
-      const readEnd = performance.now();
-      console.log('readFile performance: ', readEnd - readStart);
-
-      const fileArrayBuffer = ev.target?.result;
-      if (!fileArrayBuffer) {
-        reject('failed file buffer read');
-      }
-
-      const audioMetaData = await parseBlob(file);
-      console.log(audioMetaData);
-      resolve(audioMetaData);
-    };
-    fileReader.readAsArrayBuffer(file);
-  });
+  const audioMetaData = await parseBlob(file);
+  console.log(audioMetaData);
+  return audioMetaData;
 };
